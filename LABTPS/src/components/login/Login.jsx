a944a99745423338824a277e89fccc0e87467646
Ivo Bertoni <116576845+IvoBertoni89@users.noreply.github.com>
2024-05-06 15:00:53 -0300

    login actualizado
    nada chau errores creo

--- LABTPS/src/components/login/Login.jsx
+++ LABTPS/src/components/login/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 const LoginForm = () => {
   const [username, setUsername] = useState('');
@@ -7,39 +7,27 @@ const LoginForm = () => {
   const handleInputChange = (event) => {
     const value = event.target.value;
     setUsername(value);
+    if (value.toLowerCase().includes('o')) {
+      alert('Por favor, ¡Nombres de usuario sin la letra o!');
+    }
   };
 
   const handleRegister = () => {
     if (username.trim() === '' || username.toLowerCase().includes('o')) {
-      setErrorMessage('Nombre de Usuario no valido para registrarse, odio las o y las O >:) , mejor usa 0');
+      setErrorMessage(errorMessage = alert('“Usuario inválido para registrarse”'));
       return;
     }
-    
-    alert('¡Usuario registrado correctamente! :)');
+
+    alert('“¡Usuario registrado correctamente!”');
   };
 
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Ingrese su nombre de usuario"
-        onChange={handleInputChange}
-      />
+      <input type="text" placeholder="Ingrese su nombre de usuario" onChange={handleInputChange}/>
       <button onClick={handleRegister}>Registrarse</button>
       <p>{username}</p>
-      {errorMessage && <p>{errorMessage}</p>}
     </div>
   );
 };
 
 export default LoginForm;
-
-//import LoginForm from './components/login/Login';
-//const App = () => {
-
-  //return (
-   // <div>
-   //   <LoginForm />
-   // </div>
-  //);
-//};
